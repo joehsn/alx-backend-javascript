cb4f35e65d07db3ce3b3aa9fc7c06261b460eab7
joehsn <joehsn@outlook.com>
2024-10-03 21:47:46 +0000

    9. Hoisting corrnectly moduled

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -24,11 +24,11 @@ export class StudentHolberton {
     if (typeof firstName !== 'string') {
       throw new Error('TypeError: Firstname must be a string');
     }
-    if (typeof lastname !== 'string') {
+    if (typeof lastName !== 'string') {
       throw new Error('TypeError: Lastname must be a string');
     }
     if (!(holbertonClass instanceof HolbertonClass)) {
-      throw new Error('TypeError: class must be an instance if HolbertonClass');
+      throw new Error('TypeError: class must be an instance of HolbertonClass');
     }
     this._firstName = firstName;
     this._lastName = lastName;
@@ -57,4 +57,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
