ba1d25055a51c2932b15d427fd0108d3d43e0542
joehsn <joehsn@outlook.com>
2024-10-03 20:11:12 +0000

    Getters and Setters added to 3. Methods, static...

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -10,7 +10,29 @@ export default class Currency {
     this._name = name;
   }
 
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new Error('TypeError: Name must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('TypeError: Code must be a string');
+    }
+    this._name = value;
+  }
+
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
