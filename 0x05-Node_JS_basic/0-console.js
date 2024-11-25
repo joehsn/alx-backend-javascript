51d0c1ef052148eac3669b5fefdcf6bdc77288eb
joehsn <joehsn@outlook.com>
2024-11-25 18:12:40 +0000

    0. Executing basic javascript with Node JS task: done

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * A function that prints in STDOUT the string argument.
+ * @param {String} msg
+ * @param {void}
+ */
+
+function displayMessage(msg) {
+    console.log(msg);
+};
+
+module.exports = displayMessage;
