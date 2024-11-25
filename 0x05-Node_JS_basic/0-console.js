1fa71e7e85ab2b32ce4d34683d8efe5522b24b55
joehsn <joehsn@outlook.com>
2024-11-25 19:08:38 +0000

    0. Executing basic javascript with Node JS task: corrected

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,12 +1,10 @@
-const process = require("process");
 /**
  * A function that prints in STDOUT the string argument.
  * @param {String} msg
- * @param {void}
  */
 
 function displayMessage(msg) {
-    process.stdout.write(msg + "\n");
+    console.log(msg);
 };
 
 module.exports = displayMessage;
